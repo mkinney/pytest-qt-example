30a515e25c3fcd7bdc2a2b97f56e903b48387b2d
Mike Kinney <mike.kinney@gmail.com>
2022-02-02 10:59:35 -0800

    initial

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Simple example using pytest-qt
+
+python3 -m venv venv
+venv/bin/activate
+pip install -r requirements.txt
---
+++ test_hello.py
@@ -0,0 +1,10 @@
+from pytestqt import qtbot
+
+def test_hello(qtbot):
+    widget = HelloWidget()
+    qtbot.addWidget(widget)
+
+    # click in the Greet button and make sure it updates the appropriate label
+    qtbot.mouseClick(widget.button_greet, qt_api.QtCore.Qt.MouseButton.LeftButton)
+
+    assert widget.greet_label.text() == "Hello!"
